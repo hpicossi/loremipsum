425c10ad7b8c760d4e2bd10b09d2d5a57a121b97
HilarioP <hpicossi@gmail.com>
2023-07-18 13:53:10 -0300

    Eliminando archivos

--- git/contar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Contar líneas en los archivos loremipsum-*.txt
-for archivo in loremipsum-*.txt
-do
-	lineas=$(wc -l $archivo | cut -d' ' -f1)
-	echo "$archivo tiene $lineas líneas."
-done
